c28722b52fb8936af2df27d98843d95fab6bb051
Christopher <christopher.cruz2@upr.edu>
2018-10-17 15:46:53 -0400

    Initial setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello from express')
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is the about page')
+})
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000...')
+})
