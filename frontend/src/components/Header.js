8f666c2b80f1a30d7129f30b4aa10b774a758c79
Dervin Castro <dervin.zuriel@gmail.com>
2018-12-01 22:12:28 -0400

    changing chtistopher request

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -23,7 +23,7 @@ export default class Header extends Component {
   render() {
     return (
       <header className="header">
-        <Navbar className="fixed-top" color="dark" light expand="md">
+        <Navbar color="dark" light expand="md">
         <div className="d-flex align-items-center mr-2"><img src="images/skyline.png" alt="logo" width="60"/>  </div>
           <NavbarBrand className="text-white">Skyline Web-Systems</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
